ea5fb0a2653b009dd5e7682a92da68fdf97e0e41
Marco Scardovi <ScardracS@users.noreply.github.com>
2019-03-03 11:50:25 +0100

    Add files via upload

---
+++ PURE/Linux/flash.sh
@@ -0,0 +1,191 @@
+#! /bin/bash
+# Created by ScardracS and Licensed under the Apache License, Version 2.0 (the "License");
+# You may not use this file except in compliance with the License.
+# You may obtain a copy of the License at http://www.apache.org/licenses/LICENSE-2.0
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+echo ""
+echo "This script is developed & tested by me (ScardracS) for the Moto X Pure."
+echo "Feel free to use and/or modify, but please give me proper credits."
+echo "I don't assume any responsibility if You damage your devices, You're been warned!"
+echo ""
+echo "First of all please select your PC's OS."
+echo "Press 1 and enter for Debian & derivates."
+echo "Press 2 and enter for Fedora/Red Hat with dnf installer."
+echo "Press 3 and enter for Fedora/Red Hat with yum installer."
+echo "Press 4 and enter for ArchLinux & derivates with pacman."
+echo "Press 5 and enter for ArchLinux & derivates with yaourt."
+echo "Press any other values to stop the Script."
+read A
+case $A in
+		1)	sudo apt-get update && sudo apt-get install adb fastboot
+		;;
+		2)	sudo dnf install android-tools
+		;;
+		3)	sudo yum install android-tools
+		;;
+		4)	sudo pacman -Sy android-tools
+		;;
+		5)	sudo yaourt -Sy android-tools
+		;;
+		*)	echo "Script stopped!" && exit
+	esac
+clear
+echo "REMEBER, DON'T STOP THE FLASHING PROCESS!!"
+echo "Are You Ready? Please write Y or N"
+read B
+case $B in
+	Y) echo "Do you want to delete userdata? Please write Y/y or N/n. Any other values will stop the program!"
+		read C
+		case $C in
+		Y)
+			clear
+			echo "Wipe Script"
+			sudo adb reboot-bootloader
+			sudo fastboot oem fb_mode_set
+			sudo fastboot flash partition gpt.bin
+			sudo fastboot flash bootloader bootloader.img
+			sudo fastboot flash logo logo.bin
+			sudo fastboot flash boot boot.img
+			sudo fastboot flash recovery recovery.img
+			sudo fastboot flash system system.img_sparsechunk.0
+			sudo fastboot flash system system.img_sparsechunk.1
+			sudo fastboot flash system system.img_sparsechunk.2
+			sudo fastboot flash system system.img_sparsechunk.3
+			sudo fastboot flash system system.img_sparsechunk.4
+			sudo fastboot flash system system.img_sparsechunk.5
+			sudo fastboot flash system system.img_sparsechunk.6
+			sudo fastboot flash system system.img_sparsechunk.7
+			sudo fastboot flash system system.img_sparsechunk.8
+			sudo fastboot flash system system.img_sparsechunk.9
+			sudo fastboot flash system system.img_sparsechunk.10
+			sudo fastboot flash modem NON-HLOS.bin
+			sudo fastboot erase modemst1
+			sudo fastboot erase modemst2
+			sudo fastboot flash fsg fsg.mbn
+			sudo fastboot erase cache
+			sudo fastboot erase userdata
+			sudo fastboot erase customize
+			sudo fastboot erase clogo
+			sudo fastboot oem fb_mode_clear
+			sudo fastboot reboot
+			clear
+			exit
+		;;
+		y)
+			clear
+			echo "Wipe Script"
+			sudo adb reboot-bootloader
+			sudo fastboot oem fb_mode_set
+			sudo fastboot flash partition gpt.bin
+			sudo fastboot flash bootloader bootloader.img
+			sudo fastboot flash logo logo.bin
+			sudo fastboot flash boot boot.img
+			sudo fastboot flash recovery recovery.img
+			sudo fastboot flash system system.img_sparsechunk.0
+			sudo fastboot flash system system.img_sparsechunk.1
+			sudo fastboot flash system system.img_sparsechunk.2
+			sudo fastboot flash system system.img_sparsechunk.3
+			sudo fastboot flash system system.img_sparsechunk.4
+			sudo fastboot flash system system.img_sparsechunk.5
+			sudo fastboot flash system system.img_sparsechunk.6
+			sudo fastboot flash system system.img_sparsechunk.7
+			sudo fastboot flash system system.img_sparsechunk.8
+			sudo fastboot flash system system.img_sparsechunk.9
+			sudo fastboot flash system system.img_sparsechunk.10
+			sudo fastboot flash modem NON-HLOS.bin
+			sudo fastboot erase modemst1
+			sudo fastboot erase modemst2
+			sudo fastboot flash fsg fsg.mbn
+			sudo fastboot erase cache
+			sudo fastboot erase userdata
+			sudo fastboot erase customize
+			sudo fastboot erase clogo
+			sudo fastboot oem fb_mode_clear
+			sudo fastboot reboot
+			clear
+			exit
+		;;
+		N)
+			clear
+			echo "NoWipe Script"
+			sudo adb reboot-bootloader
+			sudo fastboot oem fb_mode_set
+			sudo fastboot flash partition gpt.bin
+			sudo fastboot flash bootloader bootloader.img
+			sudo fastboot flash logo logo.bin
+			sudo fastboot flash boot boot.img
+			sudo fastboot flash recovery recovery.img
+			sudo fastboot flash system system.img_sparsechunk.0
+			sudo fastboot flash system system.img_sparsechunk.1
+			sudo fastboot flash system system.img_sparsechunk.2
+			sudo fastboot flash system system.img_sparsechunk.3
+			sudo fastboot flash system system.img_sparsechunk.4
+			sudo fastboot flash system system.img_sparsechunk.5
+			sudo fastboot flash system system.img_sparsechunk.6
+			sudo fastboot flash system system.img_sparsechunk.7
+			sudo fastboot flash system system.img_sparsechunk.8
+			sudo fastboot flash system system.img_sparsechunk.9
+			sudo fastboot flash system system.img_sparsechunk.10
+			sudo fastboot flash modem NON-HLOS.bin
+			sudo fastboot erase modemst1
+			sudo fastboot erase modemst2
+			sudo fastboot flash fsg fsg.mbn
+			sudo fastboot erase cache
+			sudo fastboot erase customize
+			sudo fastboot erase clogo
+			sudo fastboot oem fb_mode_clear
+			sudo fastboot reboot
+			clear
+			exit
+		;;
+		n)
+			clear
+			echo "NoWipe Script"
+			sudo adb reboot-bootloader
+			sudo fastboot oem fb_mode_set
+			sudo fastboot flash partition gpt.bin
+			sudo fastboot flash bootloader bootloader.img
+			sudo fastboot flash logo logo.bin
+			sudo fastboot flash boot boot.img
+			sudo fastboot flash recovery recovery.img
+			sudo fastboot flash system system.img_sparsechunk.0
+			sudo fastboot flash system system.img_sparsechunk.1
+			sudo fastboot flash system system.img_sparsechunk.2
+			sudo fastboot flash system system.img_sparsechunk.3
+			sudo fastboot flash system system.img_sparsechunk.4
+			sudo fastboot flash system system.img_sparsechunk.5
+			sudo fastboot flash system system.img_sparsechunk.6
+			sudo fastboot flash system system.img_sparsechunk.7
+			sudo fastboot flash system system.img_sparsechunk.8
+			sudo fastboot flash system system.img_sparsechunk.9
+			sudo fastboot flash system system.img_sparsechunk.10
+			sudo fastboot flash modem NON-HLOS.bin
+			sudo fastboot erase modemst1
+			sudo fastboot erase modemst2
+			sudo fastboot flash fsg fsg.mbn
+			sudo fastboot erase cache
+			sudo fastboot erase customize
+			sudo fastboot erase clogo
+			sudo fastboot oem fb_mode_clear
+			sudo fastboot reboot
+			clear
+			exit
+		;;
+		*) 
+			clear
+			echo "Script stopped!"
+			exit
+		;;
+		esac
+	;;
+	*)
+		clear
+		echo "Script stopped!"
+		exit
+	;;
+esac
+exit
---
+++ PURE/Windows/flash.bat
@@ -0,0 +1,134 @@
+@echo off
+:WARNING
+	echo Created by ScardracS for the Moto X Pure.
+	echo I am not responsible for any damage, you're been warned!
+	echo You will be able to quit the installer simply press K on any question.
+	pause
+:START
+	cls
+	echo Have You already installed ADB and Fastboot? (Yes/No/Kill the script)
+	set /p a=
+	if /i "%a%"=="Y" GOTO ROM
+	if /i "%a%"=="N" GOTO INSTALLER
+	if /i "%a%"=="K" GOTO END
+	echo Wrong Answer!
+	GOTO START
+:INSTALLER
+	cls
+	echo This is the ADB and Fastboot's installer.
+	echo You have to install both ADB, Fastboot and Drivers.
+	echo It's better to install it System Wide.
+	pause
+	start adbinstaller.exe
+	pause
+:ROM
+	cls
+	echo Have You already downloaded the Stock ROM? (Yes/No/Kill the script)
+	set /p b=
+	if /i "%b%"=="Y" GOTO STARTINSTALL
+	if /i "%b%"=="N" GOTO DOWNLOAD
+	if /i "%b%"=="K" GOTO END
+	echo Wrong answer!
+	GOTO ROM
+:DOWNLOAD
+	cls
+	echo I'll open GitHub where You will be able to choose your preferred Stock version.
+	start https://firmware.center/firmware/Motorola/Moto%20X%20Pure%20%28XT1575%29/Stock/
+	echo WHEN THE DOWNLOAD IS FINISHED PLEASE UNZIP AND MOVE HERE ALL THE FILES.
+	pause
+:STARTINSTALL
+	cls
+	echo Are You ready to intall STOCK ROM? (Yes/No/Kill the script)
+	set /p c=
+	if /i "%c%"=="Y" GOTO READY
+	if /i "%c%"=="N" GOTO START
+	if /i "%c%"=="K" GOTO END
+	echo Wrong answer!
+	GOTO STARTINSTALL
+:READY
+	cls
+	echo Do you want to delete user data? (Yes/No/Kill the script)
+	set /p d=
+	if /i "%d%"=="Y" GOTO WIPE
+	if /i "%d%"=="N" GOTO NOWIPE
+	if /i "%d%"=="K" GOTO END
+	echo Wrong answer!
+	GOTO READY
+:WIPE
+	cls
+	echo WIPE SUBSCRIPT
+	echo DO NOT STOP THE FLASHING PROCESS!!
+	adb reboot-bootloader
+	echo FOLLOW THE INSTRUCTIONS ON WATCH ME FOLDER AND THAN HIT ENTER
+	pause
+	fastboot oem fb_mode_set
+	fastboot flash partition gpt.bin
+	fastboot flash bootloader bootloader.img
+	fastboot flash logo logo.bin
+	fastboot flash boot boot.img
+	fastboot flash recovery recovery.img
+	fastboot flash system system.img_sparsechunk.0
+	fastboot flash system system.img_sparsechunk.1
+	fastboot flash system system.img_sparsechunk.2
+	fastboot flash system system.img_sparsechunk.3
+	fastboot flash system system.img_sparsechunk.4
+	fastboot flash system system.img_sparsechunk.5
+	fastboot flash system system.img_sparsechunk.6
+	fastboot flash system system.img_sparsechunk.7
+	fastboot flash system system.img_sparsechunk.8
+	fastboot flash modem NON-HLOS.bin
+	fastboot erase modemst1 
+	fastboot erase modemst2 
+	fastboot flash fsg fsg.mbn
+	fastboot erase cache 
+	fastboot erase userdata
+	fastboot erase customize
+	fastboot erase clogo
+	fastboot oem fb_mode_clear
+	GOTO LOOK
+:NOWIPE
+	cls
+	echo NOWIPE SUBSCRIPT
+	echo DO NOT STOP THE FLASHING PROCESS!!
+	adb reboot-bootloader
+	echo FOLLOW THE INSTRUCTIONS ON WATCH ME FOLDER AND THAN HIT ENTER
+	pause
+	fastboot oem fb_mode_set
+	fastboot flash partition gpt.bin
+	fastboot flash bootloader bootloader.img
+	fastboot flash logo logo.bin
+	fastboot flash boot boot.img
+	fastboot flash recovery recovery.img
+	fastboot flash system system.img_sparsechunk.0
+	fastboot flash system system.img_sparsechunk.1
+	fastboot flash system system.img_sparsechunk.2
+	fastboot flash system system.img_sparsechunk.3
+	fastboot flash system system.img_sparsechunk.4
+	fastboot flash system system.img_sparsechunk.5
+	fastboot flash system system.img_sparsechunk.6
+	fastboot flash system system.img_sparsechunk.7
+	fastboot flash system system.img_sparsechunk.8
+	fastboot flash modem NON-HLOS.bin
+	fastboot erase modemst1 
+	fastboot erase modemst2 
+	fastboot flash fsg fsg.mbn
+	fastboot erase cache 
+	fastboot erase customize
+	fastboot erase clogo
+	fastboot oem fb_mode_clear
+:LOOK
+	echo Do you want to lock your device? (Yes/No)
+	set /p e=
+	if /i "%d%"=="Y" GOTO LOCK
+	if /i "%d%"=="N" GOTO END
+	echo Wrong answer!
+	GOTO READY
+:LOCK
+	echo Locking device
+	fastboot oem lock
+	echo Device locked
+:END
+	fastboot reboot
+	cls
+	echo Enjoy! :D
+	pause
