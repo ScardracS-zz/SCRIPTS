69ade605a5a366c2a696f32fe4d5cdb61e49e3c3
Marco Scardovi <ScardracS@users.noreply.github.com>
2019-03-04 13:07:12 +0100

    Update Logcat.bat

--- WINDOWS/Logcat.bat
+++ WINDOWS/Logcat.bat
@@ -4,7 +4,7 @@
 	echo You can use and/or modify it, but please give me proper credit.
 :START
 	echo.
-	echo Have You installed ADB? (Yes/No/Kill the script)
+	echo Have You installed ADB? (Y/N/K)
 	set /p a=
 	if /i "%a%"=="Y" GOTO LOGCAT
 	if /i "%a%"=="N" GOTO INSTALLER
@@ -15,27 +15,25 @@
 	cls
 	echo I'll install ADB
 	echo Please install both ADB, Fatsboot and Drivers.
-	start adbinstaller.exe
+	start https://github.com/hackslashX/ADB-Fastboot-Installer-for-Windows/raw/master/versions/adb_fastboot_installer-1.2s.exe
+	start adb_fastboot_installer-1.2s.exe
 	pause
 :LOGCAT
 	cls
-	echo Do you want to overwrite previous file? (Yes/No/Kill the script)
+	echo Do you want to overwrite previous file? (Y/N/K)
 	set /p b=
-	if /i "%b%"=="Y" GOTO OVER
-	if /i "%b%"=="N" GOTO NOVER
+	if /i "%b%"=="Y" GOTO OVERWRITE
+	if /i "%b%"=="N" GOTO NO_OVERWRITE
 	if /i "%b%"=="K" GOTO END
 	echo Wrong Answer!
 	GOTO LOGCAT
-:OVER
+:OVERWRITE
 	cls
 	adb logcat > logcat.txt
 	GOTO END
-:NOVER
+:NO_OVERWRITE
 	cls
 	adb logcat >> logcat.txt
-:END
-	cls
-	pause
 :END
 	cls
 	pause
