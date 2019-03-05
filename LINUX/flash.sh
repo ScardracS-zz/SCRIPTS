ad6e83b124f1999d113cb490fa6ca8c3d4a16e7a
ScardracS <ScardracS@users.noreply.github.com>
2019-03-05 12:59:03 +0100

    Update flash.sh
    Added sperimental support for Gentoo linux and other OS's with emerge package manager

--- LINUX/flash.sh
+++ LINUX/flash.sh
@@ -8,16 +8,17 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 echo ""
-echo "This script is developed & tested by me (ScardracS) for the Moto X Play/Pure/Style."
-echo "Feel free to use and/or modify, but please give me proper credits."
+echo "This script is developed & tested by me (ScardracS) for the Motorola Moto X Play/Pure/Style."
+echo "Feel free to use and/or modify it, but please give me proper credits."
 echo "I don't assume any responsibility if You damage your device/s, You have been warned!"
 echo ""
 echo "First of all please select your PC's OS."
-echo "Press 1 and enter for Debian & derivates."
-echo "Press 2 and enter for Fedora/Red Hat with dnf installer."
-echo "Press 3 and enter for Fedora/Red Hat with yum installer."
-echo "Press 4 and enter for ArchLinux & derivates with pacman."
-echo "Press 5 and enter for ArchLinux & derivates with yaourt."
+echo "Press 1 and enter if you have apt package manager."
+echo "Press 2 and enter if you have dnf package manager."
+echo "Press 3 and enter if you have yum package manager."
+echo "Press 4 and enter if you have pacman package manager."
+echo "Press 5 and enter if you have yaourt package manager."
+echo "Press 6 and enter if you have emerge package manager."
 echo "Press any other values to stop the Script and exit."
 read A
 case $A in
@@ -31,6 +32,8 @@ case $A in
 		;;
 		5)	sudo yaourt -Sy android-tools
 		;;
+		6)	sudo emerge --ask android-sdk-update-manager
+		;;
 		*)	echo "Script stopped!" && exit
 	esac
 clear
@@ -38,81 +41,82 @@ echo "REMEBER, DON'T STOP THE FLASHING PROCESS!!"
 echo "Are You Ready? (y/n)"
 read B
 case $B in
-	y) echo "Do you want to delete userdata? Please write y or n. Any other values will stop the program!"
+	y) 
+		echo "Do you want to delete userdata? (y/n)"
 		read C
 		case $C in
-		y)
-			clear
-			echo "Wipe Script"
-			sudo adb reboot-bootloader
-			sudo fastboot oem fb_mode_set
-			sudo fastboot flash partition gpt.bin
-			sudo fastboot flash bootloader bootloader.img
-			sudo fastboot flash logo logo.bin
-			sudo fastboot flash boot boot.img
-			sudo fastboot flash recovery recovery.img
-			sudo fastboot flash system system.img_sparsechunk.0
-			sudo fastboot flash system system.img_sparsechunk.1
-			sudo fastboot flash system system.img_sparsechunk.2
-			sudo fastboot flash system system.img_sparsechunk.3
-			sudo fastboot flash system system.img_sparsechunk.4
-			sudo fastboot flash system system.img_sparsechunk.5
-			sudo fastboot flash system system.img_sparsechunk.6
-			sudo fastboot flash system system.img_sparsechunk.7
-			sudo fastboot flash system system.img_sparsechunk.8
-			sudo fastboot flash system system.img_sparsechunk.9
-			sudo fastboot flash system system.img_sparsechunk.10
-			sudo fastboot flash modem NON-HLOS.bin
-			sudo fastboot erase modemst1
-			sudo fastboot erase modemst2
-			sudo fastboot flash fsg fsg.mbn
-			sudo fastboot erase cache
-			sudo fastboot erase userdata
-			sudo fastboot erase customize
-			sudo fastboot erase clogo
-			sudo fastboot oem fb_mode_clear
-			sudo fastboot reboot
-			clear
-			exit
-		;;
-		n)
-			clear
-			echo "NoWipe Script"
-			sudo adb reboot-bootloader
-			sudo fastboot oem fb_mode_set
-			sudo fastboot flash partition gpt.bin
-			sudo fastboot flash bootloader bootloader.img
-			sudo fastboot flash logo logo.bin
-			sudo fastboot flash boot boot.img
-			sudo fastboot flash recovery recovery.img
-			sudo fastboot flash system system.img_sparsechunk.0
-			sudo fastboot flash system system.img_sparsechunk.1
-			sudo fastboot flash system system.img_sparsechunk.2
-			sudo fastboot flash system system.img_sparsechunk.3
-			sudo fastboot flash system system.img_sparsechunk.4
-			sudo fastboot flash system system.img_sparsechunk.5
-			sudo fastboot flash system system.img_sparsechunk.6
-			sudo fastboot flash system system.img_sparsechunk.7
-			sudo fastboot flash system system.img_sparsechunk.8
-			sudo fastboot flash system system.img_sparsechunk.9
-			sudo fastboot flash system system.img_sparsechunk.10
-			sudo fastboot flash modem NON-HLOS.bin
-			sudo fastboot erase modemst1
-			sudo fastboot erase modemst2
-			sudo fastboot flash fsg fsg.mbn
-			sudo fastboot erase cache
-			sudo fastboot erase customize
-			sudo fastboot erase clogo
-			sudo fastboot oem fb_mode_clear
-			sudo fastboot reboot
-			clear
-			exit
-		;;
-		*)
-			clear
-			echo "Script stopped!"
-			exit
-		;;
+			y)
+				clear
+				echo "Wipe Script"
+				sudo adb reboot-bootloader
+				sudo fastboot oem fb_mode_set
+				sudo fastboot flash partition gpt.bin
+				sudo fastboot flash bootloader bootloader.img
+				sudo fastboot flash logo logo.bin
+				sudo fastboot flash boot boot.img
+				sudo fastboot flash recovery recovery.img
+				sudo fastboot flash system system.img_sparsechunk.0
+				sudo fastboot flash system system.img_sparsechunk.1
+				sudo fastboot flash system system.img_sparsechunk.2
+				sudo fastboot flash system system.img_sparsechunk.3
+				sudo fastboot flash system system.img_sparsechunk.4
+				sudo fastboot flash system system.img_sparsechunk.5
+				sudo fastboot flash system system.img_sparsechunk.6
+				sudo fastboot flash system system.img_sparsechunk.7
+				sudo fastboot flash system system.img_sparsechunk.8
+				sudo fastboot flash system system.img_sparsechunk.9
+				sudo fastboot flash system system.img_sparsechunk.10
+				sudo fastboot flash modem NON-HLOS.bin
+				sudo fastboot erase modemst1
+				sudo fastboot erase modemst2
+				sudo fastboot flash fsg fsg.mbn
+				sudo fastboot erase cache
+				sudo fastboot erase userdata
+				sudo fastboot erase customize
+				sudo fastboot erase clogo
+				sudo fastboot oem fb_mode_clear
+				sudo fastboot reboot
+				clear
+				exit
+			;;
+			y)
+				clear
+				echo "Wipe Script"
+				sudo adb reboot-bootloader
+				sudo fastboot oem fb_mode_set
+				sudo fastboot flash partition gpt.bin
+				sudo fastboot flash bootloader bootloader.img
+				sudo fastboot flash logo logo.bin
+				sudo fastboot flash boot boot.img
+				sudo fastboot flash recovery recovery.img
+				sudo fastboot flash system system.img_sparsechunk.0
+				sudo fastboot flash system system.img_sparsechunk.1
+				sudo fastboot flash system system.img_sparsechunk.2
+				sudo fastboot flash system system.img_sparsechunk.3
+				sudo fastboot flash system system.img_sparsechunk.4
+				sudo fastboot flash system system.img_sparsechunk.5
+				sudo fastboot flash system system.img_sparsechunk.6
+				sudo fastboot flash system system.img_sparsechunk.7
+				sudo fastboot flash system system.img_sparsechunk.8
+				sudo fastboot flash system system.img_sparsechunk.9
+				sudo fastboot flash system system.img_sparsechunk.10
+				sudo fastboot flash modem NON-HLOS.bin
+				sudo fastboot erase modemst1
+				sudo fastboot erase modemst2
+				sudo fastboot flash fsg fsg.mbn
+				sudo fastboot erase cache
+				sudo fastboot erase customize
+				sudo fastboot erase clogo
+				sudo fastboot oem fb_mode_clear
+				sudo fastboot reboot
+				clear
+				exit
+			;;
+			*)
+				clear
+				echo "Script stopped!"
+				exit
+			;;
 		esac
 	;;
 	*)
